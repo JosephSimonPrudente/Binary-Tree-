8a54de56940fb6ce0fb65d1ed68cc1ed8caec7ad
JosephSimonPrudente <prudentejoseph3@gmail.com>
2023-01-14 21:52:52 +0800

    adding search on the my name binary

---
+++ Search name.py
@@ -0,0 +1,75 @@
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self, data):
+        if data == self.data:
+            return 
+
+        if data < self.data:
+
+            if self.left:
+                self.left.add_child(data)
+            else: 
+                self.left = BinarySearchTreeNode(data)
+
+        else:
+        
+            if self.right:
+                self.right.add_child(data)
+            else: 
+                self.right = BinarySearchTreeNode(data)
+    
+    def in_order_traversal(self):
+        elements =[]
+
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        elements.append(self.data)
+
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements
+        
+    def search(self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+    
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+    print("NAME:",elements)
+
+    for i in range(1, len(elements)):
+        root.add_child(elements[i])
+    
+    return root
+
+if __name__ == '__main__':
+    Name = ("J","O","S","E","P","H","S","I","M","O","N","S","P","R","U","D","E","N","T","E")
+    name_tree = build_tree(Name)
+    print("In order Traversal:",name_tree.in_order_traversal())
+
+    print("is Z on the list?", name_tree.search("Z"))
+    print("is S on the list?", name_tree.search("S"))
+
+  
+
+
+
+
