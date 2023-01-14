f380de968eecf6961f58cf176af656dc320d5cac
JosephSimonPrudente <prudentejoseph3@gmail.com>
2023-01-14 21:17:07 +0800

    adding build_tree and my Name and printing it

--- My name.py
+++ My name.py
@@ -32,5 +32,21 @@ class BinarySearchTreeNode:
 
         if self.right:
             elements += self.left.in_order_traversal()
+    
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range(1, len(elements)):
+        root.add_child(elements[i])
+    print("My Name:",elements)
+    return root
+
+if __name__ == '__main__':
+    Name = ("J","O","S","E","P","H","S","I","M","O","N","S","P","R","U","D","E","N","T","E")
+    name_tree = build_tree(Name)
+    
+  
+
+
 
 
